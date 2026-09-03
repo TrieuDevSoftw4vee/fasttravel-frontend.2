5f6bd93409e3be4152833e97e9b93802ffb679da
nguyentuzan1234-ship-it <nguyentuzan1234@gmail.com>
2026-09-03 23:01:23 +0700

    Add files via upload

---
+++ src/api/accountApi.js
@@ -0,0 +1,1 @@
+import {api} from './client.js';export const accountApi={bookings:()=>api('/bookings')};
---
+++ src/components/ToastMessage.vue
@@ -0,0 +1,2 @@
+<script setup>import {useToast} from '../stores/toast.js';const{message}=useToast();</script>
+<template><div v-if="message" class="toast" @click="message=''">{{message}}</div></template>
